55e6bb7b66354913e592362579137b2331a9c235
Jefferson Christian <jeffchristian.webdesigner@gmail.com>
2025-05-12 15:41:34 -0300

    connect our project with mongoose to mongoDB and iniciate a model for a Course Schema

---
+++ model/courseModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const Course =
+  mongoose.models.Course ?? mongoose.model("Course", courseSchema);
